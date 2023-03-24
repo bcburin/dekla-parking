021626d85db7d25a60d0cb4a6a70586ca80a5ee8
bcburin <cburin.bruno@gmail.com>
2023-03-24 12:34:24 -0300

    Refactor user api using fastapi_restfil CBV

--- server/api/v1/endpoints/user.py
+++ server/api/v1/endpoints/user.py
@@ -1,111 +1,65 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy.exc import IntegrityError
+from fastapi_restful.cbv import cbv
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
 
 from server.database.config import DeklaParkingDb
 from server.database.user import UserCRUD
 from server.schemas.user import UserOut, UserCreate, UserUpdate
 
-router = APIRouter(
-    prefix='/users',
-    tags=['users'],
-)
+router = APIRouter(prefix='/users', tags=['users'])
 
 
-@router.get(
-    path='/',
-    response_model=list[UserOut],
-)
-def get_users(
-        skip: int = Query(
-            default=0,
-            ge=0,
-            description='number of users to skip sequentially in the database'
-        ),
-        limit: int | None = Query(
-            default=100,
-            ge=0,
-            description='maximum number of users to return '
-                        '(may be set to null to return all users in database)'
-        ),
-        db=Depends(DeklaParkingDb.get_db)
-):
-    return UserCRUD(db).get_all(skip=skip, limit=limit)
+@cbv(router)
+class UserCBV:
+    db = Depends(DeklaParkingDb.get_db)
 
+    @router.get(path='/', response_model=list[UserOut])
+    def get_all_users(self, skip: int = Query(default=0, ge=0), limit: int | None = Query(default=100, ge=0)):
+        return UserCRUD(self.db).get_all(skip=skip, limit=limit)
 
-@router.get(
-    path='/{user_id}',
-    response_model=UserOut,
-    responses={404: {'detail': 'No such user.'}}
-)
-def get_user_by_id(user_id: int, db=Depends(DeklaParkingDb.get_db)):
-    user = UserCRUD(db).get_by_id(user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail='No such user.')
-    return user
+    @router.get(path='/{user_id}', response_model=UserOut)
+    def get_user_by_id(self, user_id: int):
+        user = UserCRUD(self.db).get_by_id(user_id)
+        if not user:
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+        return user
 
+    @router.get(path='/find', response_model=UserOut)
+    def get_user_by_index(self, username: str | None = None, email: str | None = None):
+        if not username and not email:
+            raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail='A username or email must be specified.')
+        user = None
+        if username:
+            user = UserCRUD(self.db).get_by_username(username)
+        elif email:
+            user = UserCRUD(self.db).get_by_email(email)
+        if not user:
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+        return user
 
-@router.get(
-    path='/find',
-    response_model=UserOut,
-    responses={
-        404: {'detail': 'No such user.'},
-        400: {'detail': 'A username or email must be specified.'}
-    }
-)
-def get_user_by_index(username: str | None = None, email: str | None = None, db=Depends(DeklaParkingDb.get_db)):
-    if not username and not email:
-        raise HTTPException(status_code=400, detail='A username or email must be specified.')
-    user = None
-    if username:
-        user = UserCRUD(db).get_by_username(username)
-    elif email:
-        user = UserCRUD(db).get_by_email(email)
-    if not user:
-        raise HTTPException(status_code=404, detail='No such user.')
-    return user
-
-
-@router.post(
-    path='/',
-    response_model=UserOut,
-    responses={400: {'detail': 'DB error string'}}
-)
-def create_user(user: UserCreate, db=Depends(DeklaParkingDb.get_db)):
-    try:
-        created_user = UserCRUD(db).create(user=user)
+    @router.post(path='/', response_model=UserOut)
+    def create_user(self, user: UserCreate):
+        user_db = UserCRUD(self.db).get_by_username(username=user.username)
+        if user_db:
+            raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f'{user_db.username} already exists')
+        created_user = UserCRUD(self.db).create(user=user)
         return created_user
-    except IntegrityError as dbe:
-        raise HTTPException(status_code=400, detail=dbe.orig.args)
 
+    @router.put(path='/{user_id}', response_model=UserOut)
+    def update_user(self, user_id: int, user: UserUpdate):
+        db_user, authenticated = UserCRUD(self.db).authenticate(id=user_id, password=user.password)
+        if not authenticated:
+            raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail='Password authentication failed.')
+        if not db_user:
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+        if not user.has_updates():
+            raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail='No updates provided.')
+        updated_user = UserCRUD(self.db).update(db_user=db_user, user=user)
+        return updated_user
 
-@router.put(
-    path='/{user_id}',
-    response_model=UserOut,
-    responses={
-        404: {'detail': 'No such user.'},
-        401: {'detail': 'Password authentication is required.'},
-        400: {'detail': 'No updates provided.'}
-    },
-)
-def update_user(user_id: int, user: UserUpdate, db=Depends(DeklaParkingDb.get_db)):
-    db_user, authenticated = UserCRUD(db).authenticate(id=user_id, password=user.password)
-    if not authenticated:
-        raise HTTPException(status_code=401, detail='Password authentication failed.')
-    if not db_user:
-        raise HTTPException(status_code=404, detail='No such user.')
-    if not user.has_updates():
-        raise HTTPException(status_code=400, detail='No updates provided.')
-    updated_user = UserCRUD(db).update(db_user=db_user, user=user)
-    return updated_user
-
-
-@router.delete(
-    path='/{user_id}',
-    response_model=UserOut,
-    responses={404: {'detail': 'No such user'}}
-)
-def delete_user(user_id: int, db=Depends(DeklaParkingDb.get_db)):
-    deleted_user = UserCRUD(db).remove(pk=user_id)
-    if not deleted_user:
-        raise HTTPException(status_code=404, detail='No such user.')
-    return deleted_user
+    @router.delete(path='/{user_id}', response_model=UserOut)
+    def delete_user(self, user_id: int):
+        deleted_user = UserCRUD(self.db).remove(pk=user_id)
+        if not deleted_user:
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+        return deleted_user
