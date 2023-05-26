cfff4d7735c272f998f7fa4ae705d48234bbab7a
bcburin <cburin.bruno@gmail.com>
2023-05-26 00:54:27 -0300

    [back] fix current_timestamp bug

--- server/common/models/base.py
+++ server/common/models/base.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 class BaseModel(DeclarativeBase):
     created_at: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
     updated_at: Mapped[datetime] = mapped_column(
-        server_default=func.current_timestamp(), onupdate=func.current_timestamp)
+        server_default=func.current_timestamp(), onupdate=func.current_timestamp())
 
 
 class IntervalModel:
