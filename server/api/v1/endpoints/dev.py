2359d00fb00bdfd29c26b11a0977791b03f88360
bcburin <cburin.bruno@gmail.com>
2023-05-30 09:42:45 -0300

    [back] require authentication to use data generator

--- server/api/v1/endpoints/dev.py
+++ server/api/v1/endpoints/dev.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from server.common.exceptions.auth import AuthException
 from server.common.schemas.generator import GenerationRequest, GenerationResponse
 from server.database.config import get_db
+from server.services.auth import AuthReq
 from server.services.booking import BookingService
 from server.services.label import LabelService
 from server.services.labeling import LabelingService
@@ -16,7 +17,7 @@ from server.services.user import UserService
 router = APIRouter(prefix='/data-generator', tags=['development'])
 
 
-@router.post('/', response_model=GenerationResponse)
+@router.post('/', response_model=GenerationResponse, dependencies=[Depends(AuthReq.current_user_has_permission)])
 def generate_mock_data(req: GenerationRequest, db: Session = Depends(get_db)):
     if not getenv('DEV_MODE'):
         raise AuthException('Data generation only available in dev mode.')
