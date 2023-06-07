1c5894267192f1e4b13dc67998b68624f85c201b
caruso44 <91800368+caruso44@users.noreply.github.com>
2023-06-06 22:26:23 -0300

    start to create policy screen

---
+++ client/src/pages/exclusive_policy.js
@@ -0,0 +1,96 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import { useCallback, useEffect, useState } from "react";
+
+import ActionsToolbar from "src/components/actions-toolbar";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import Head from "next/head";
+import exclusivePolicyAPI from "src/api/exclusive_policy";
+
+const Page = () => {
+  const [exclusivePolicies, setExclusivePolicies] = useState([]);
+  const [deleteModalState, setDeleteModalState] = useState({
+    isOpen: false,
+    exclusivePolicy: null,
+  });
+  const [deleteManyModelIsOpen, setDeleteManyModalIsOpen] = useState(false);
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const getExclusivePoliciesHandler = async () => {
+    try {
+      const exclusivePolicy = await exclusivePolicyAPI.getEntities();
+      setExclusivePolicies(exclusivePolicy);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getExclusivePoliciesHandler();
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "descriptor", headerName: "Descriptor", width: 150 },
+    { field: "price", headerName: "Price", width: 150 },
+    {
+      field: "createdAt",
+      headerName: "Creation",
+      width: 150,
+      type: "date",
+      valueGetter: ({ value }) => value && new Date(value),
+    },
+    {
+      field: "updatedAt",
+      headerName: "Last Update",
+      width: 200,
+      type: "dateTime",
+      valueGetter: ({ value }) => value && new Date(value),
+    },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Exclusive Policy | Dekla Parking</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack direction="row" justifyContent="space-between" spacing={4}>
+              <Stack spacing={1}>
+                <Typography variant="h4">Exclusive Policy</Typography>
+                <Stack alignItems="center" direction="row" spacing={1}>
+                  <DataGrid
+                    rows={exclusivePolicies}
+                    columns={columns}
+                    components={{ Toolbar: ActionsToolbar }}
+                    componentsProps={{
+                      toolbar: {
+                        deleteIsDisabled: selectedRows.length == 0,
+                      },
+                    }}
+                    checkboxSelection
+                    disableRowSelectionOnClick
+                    onRowSelectionModelChange={(ids) => setSelectedRows(ids)}
+                  />
+                </Stack>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
---
+++ client/src/pages/public_policy.js
@@ -0,0 +1,91 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import { useCallback, useEffect, useState } from "react";
+
+import ActionsToolbar from "src/components/actions-toolbar";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import Head from "next/head";
+import publicPolicyAPI from "src/api/public_policy";
+
+
+const Page = () => {
+  const [publicPolicies, setPublicPolicy] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const getPublicPolicyHandler = async () => {
+    try {
+      const publicPolicies = await publicPolicyAPI.getEntities();
+      setPublicPolicy(publicPolicies);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getPublicPolicyHandler();
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "name", headerName: "Policyname", width: 150 },
+    { field: "descriptor", headerName: "descriptor", width: 200 },
+    { field: "price", headerName: "price", width: 150 },
+    {
+      field: "createdAt",
+      headerName: "Creation",
+      width: 150,
+      type: "date",
+      valueGetter: ({ value }) => value && new Date(value),
+    },
+    {
+      field: "updatedAt",
+      headerName: "Last Update",
+      width: 200,
+      type: "dateTime",
+      valueGetter: ({ value }) => value && new Date(value),
+    },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Public Policy | Dekla Parking</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack direction="row" justifyContent="space-between" spacing={4}>
+              <Stack spacing={1}>
+                <Typography variant="h4">publicPolicies</Typography>
+                <Stack alignItems="center" direction="row" spacing={1}>
+                  <DataGrid
+                    rows={publicPolicies}
+                    columns={columns}
+                    components={{ Toolbar: ActionsToolbar }}
+                    componentsProps={{
+                      toolbar: {
+                        deleteIsDisabled: selectedRows.length == 0,
+                      },
+                    }}
+                    checkboxSelection
+                    disableRowSelectionOnClick
+                    onRowSelectionModelChange={(ids) => setSelectedRows(ids)}
+                  />
+                </Stack>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
